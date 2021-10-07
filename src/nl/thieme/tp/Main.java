34819758f9bc08a004b68acdca7411a7f4fcecfc
Thieme <thieme@hoogernet.nl>
2021-10-07 21:34:27 +0200

    Inital commit

---
+++ src/nl/thieme/tp/Main.java
@@ -0,0 +1,17 @@
+package nl.thieme.tp;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+
+
+    public void onEnable() {
+
+    }
+
+
+    public void onDisable() {
+
+    }
+
+}
